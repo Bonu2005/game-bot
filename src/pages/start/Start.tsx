2f0724a3af0691dcd15d6b20832ead94e4d8c8ce
Bonu2005 <booonu@icloud.com>
2025-08-20 01:06:26 +0500

    game

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation, Link } from "react-router-dom";
-import axios from "axios";
+
+import { Link } from "react-router-dom";
 
 import image3 from "../../assets/imgs/image 3.svg";
 import shareplay from "../../assets/imgs/shareplay.svg";
@@ -25,15 +24,15 @@ const Start = () => {
         <img src={shareplay} alt="Invite" className="w-5 h-5 mr-2" />
         <p className="text-black text-[16px] font-medium">Invite friendss</p>
       </div>
-<Link to={"/chooseLevel"}  className="flex items-center justify-center w-full max-w-[320px] h-[52px] rounded-full bg-[#FFA500] shadow-md"> 
- 
+      <Link to={"/chooseLevel"} className="flex items-center justify-center w-full max-w-[320px] h-[52px] rounded-full bg-[#FFA500] shadow-md">
+
         <img src={play} alt="Play" className="w-6 h-6 mr-2" />
         <p className="text-white text-[16px] font-semibold">
-          {  "Play"}
+          {"Play"}
         </p>
-    
-</Link>
-   
+
+      </Link>
+
     </div>
   );
 };
