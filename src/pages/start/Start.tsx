28867918fc874816196baeec0d2894954e6a3a97
Bonu2005 <booonu@icloud.com>
2025-08-21 20:55:13 +0500

    game

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -3,33 +3,43 @@ import image3 from "../../assets/imgs/image 3.svg";
 import shareplay from "../../assets/imgs/shareplay.svg";
 import play from "../../assets/imgs/play.circle.fill.svg";
 
-
 const Start = () => {
-
   const location = useLocation();
-  const state = location.state as { telegramId?: string; username?: string; sessionId?: string };
+  const state = location.state as {
+    telegramId?: string;
+    username?: string;
+    sessionId?: string;
+  };
   const { telegramId, username, sessionId } = state;
 
+  const handleInvite = () => {
+    const gameUrl = "https://t.me/WordEngUz_bot?game=english";
+    const text = "Check out this cool game!";
+    const shareLink = `https://t.me/share/url?url=${encodeURIComponent(
+      gameUrl
+    )}&text=${encodeURIComponent(text)}`;
+
+    window.open(shareLink, "_blank");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center px-6">
-      <h1 className="text-white font-bold text-[24px] mb-6">Word Quiz{username}</h1>
+      <h1 className="text-white font-bold text-[24px] mb-6">
+        Word Quiz {username}
+      </h1>
 
       <img src={image3} alt="Quiz" className="w-[220px] h-auto mb-10" />
 
+      {/* Invite button */}
       <div
         className="flex items-center justify-center w-full max-w-[320px] h-[52px] rounded-full bg-white mb-4 shadow-md cursor-pointer"
-        onClick={() =>
-          window.open(
-            "https://t.me/WordEngUz_bot?game=english&text=Check out this cool game!",
-            "_blank"
-          )
-        }
+        onClick={handleInvite}
       >
         <img src={shareplay} alt="Invite" className="w-5 h-5 mr-2" />
         <p className="text-black text-[16px] font-medium">Invite friends</p>
       </div>
 
+      {/* Play button */}
       <Link
         to="/chooseLevel"
         state={{ telegramId, username, sessionId }}
