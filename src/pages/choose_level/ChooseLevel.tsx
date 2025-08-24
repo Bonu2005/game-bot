d794d2b84b60753f7c83ececb46edfdc17a3a32e
Bonu2005 <booonu@icloud.com>
2025-08-24 13:22:54 +0500

    game

--- src/pages/choose_level/ChooseLevel.tsx
+++ src/pages/choose_level/ChooseLevel.tsx
@@ -56,7 +56,7 @@ const ChooseLevel = () => {
   return (
     <div className="flex flex-col items-center py-10 px-6">
       <h1 className="text-white font-bold text-[24px] mb-8">
-        Choose your level {username}
+        Choose your level {sessionId}
       </h1>
       <div className="grid grid-cols-2 gap-x-5 gap-y-6">
         {levels.map((level) => (
