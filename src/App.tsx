ae6605f098cb0097ac0dfde8cc7449dd877b8bbc
Bonu2005 <booonu@icloud.com>
2025-08-25 19:40:13 +0500

    game

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
  
-import { useEffect } from 'react';
-import MainRouter from './pages'
+// import { useEffect } from 'react';
+// import MainRouter from './pages'
 
-function App() {
-    useEffect(() => {
-    const audio = new Audio("/sounds/home.mp3"); // файл в public/sounds/home.mp3
-    audio.loop = true;
-    audio.volume = 0.5;
+import { useTelegram } from "./hooks/UseTelegram";
 
-    // пробуем запустить
-    audio.play().catch(() => {
-      console.log("Автовоспроизведение заблокировано, ждём клика пользователя");
-    });
+// function App() {
+//     useEffect(() => {
+//     const audio = new Audio("/sounds/home.mp3"); // файл в public/sounds/home.mp3
+//     audio.loop = true;
+//     audio.volume = 0.5;
 
-    return () => {
-      audio.pause();
-      audio.currentTime = 0;
-    };
-  }, []);
+//     // пробуем запустить
+//     audio.play().catch(() => {
+//       console.log("Автовоспроизведение заблокировано, ждём клика пользователя");
+//     });
+
+//     return () => {
+//       audio.pause();
+//       audio.currentTime = 0;
+//     };
+//   }, []);
    
 
-  return (
-    <>
-     <MainRouter/>
-    </>
-  )
-}
+//   return (
+//     <>
+//      <MainRouter/>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
+
+
+
+
+const App = () => {
+  const { userId, firstName, lastName, username } = useTelegram();
+
+  return (
+    <div>
+      <p>Sizning chat ID: {userId}</p>
+      <p>Sizning firstName: {firstName}</p>
+      {lastName && <p>Sizning lastName: {lastName}</p>}
+      {username && <p>Sizning username: {username}</p>}
+    </div>
+  );
+};
+
+export default App;
